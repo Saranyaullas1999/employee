2b9847b195806bdc91fdbb6de8b867b1a633fc1a
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-13 15:22:15 +0530

    forms

--- src/app/addemployee/addemployee.component.ts
+++ src/app/addemployee/addemployee.component.ts
@@ -9,6 +9,39 @@ export class AddemployeeComponent implements OnInit {
 
   constructor() { }
 
+  employeeCode=""
+  employeeName=""
+  desigination=""
+  gender=""
+  companyName=""
+  salary=""
+  address=""
+  mobileno=""
+  email=""
+  year=""
+  bloodgroup=""
+  dob=""
+
+
+  readValues=()=>{
+    let data={
+      "employeeCode":this.employeeCode,
+  "employeeName":this.employeeName,
+  "desigination":this.desigination,
+  "gender":this.gender,
+  "companyName":this.companyName,
+  "salary":this.salary,
+  "address":this.address,
+ "mobileno":this.mobileno,
+  "email":this.email,
+  "year":this.year,
+  "bloodgroup":this.bloodgroup,
+  "dob":this.dob
+    }
+    console.log(data)
+  }
+
+
   ngOnInit(): void {
   }
 
